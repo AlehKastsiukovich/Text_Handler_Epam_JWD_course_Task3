2e75f86655f6273843bded8b10d3e7f85eed271f
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-02-23 19:53:53 +0300

    Add method checkParseMethodParametr to ParseValidator.class

---
+++ src/main/java/by/epam/javatraining/texthandler/validator/ParserValidator.java
@@ -0,0 +1,28 @@
+package by.epam.javatraining.texthandler.validator;
+
+import by.epam.javatraining.texthandler.entity.TextComponent;
+import by.epam.javatraining.texthandler.exception.ServiceException;
+
+public class ParserValidator {
+
+    private ParserValidator() {
+    }
+
+    private static class TextComponentValidatorHolder {
+        private static final ParserValidator INSTANCE = new ParserValidator();
+    }
+
+    public static ParserValidator getInstance() {
+        return TextComponentValidatorHolder.INSTANCE;
+    }
+
+    public void checkParseMethodParameters(TextComponent textComponent, String text) throws ServiceException {
+        if (textComponent != null && textComponent.getTextComponentType() != null) {
+            throw new ServiceException();
+        }
+
+        if (text != null && text.length() != 0) {
+            throw new ServiceException();
+        }
+    }
+}
