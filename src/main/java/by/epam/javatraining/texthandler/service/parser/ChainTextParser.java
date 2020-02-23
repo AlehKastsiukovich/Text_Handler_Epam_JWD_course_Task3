e13970ba18c3f4bbec7c3edc4d1cee5d2e97e4d5
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-02-23 19:19:00 +0300

    Add Logger and validation to ParagraphToSentenceParser class

--- src/main/java/by/epam/javatraining/texthandler/service/parser/ChainTextParser.java
+++ src/main/java/by/epam/javatraining/texthandler/service/parser/ChainTextParser.java
@@ -1,8 +1,9 @@
 package by.epam.javatraining.texthandler.service.parser;
 
 import by.epam.javatraining.texthandler.entity.TextComponent;
+import by.epam.javatraining.texthandler.exception.ServiceException;
 
 public interface ChainTextParser {
-    void setNextParser(ChainTextParser parser);
-    TextComponent parse(TextComponent textComponent, String text);
+    void setNextParser(ChainTextParser parser) throws ServiceException;
+    TextComponent parse(TextComponent textComponent, String text) throws ServiceException;
 }
