52dfb995b691c31721536e51e78b93b7119e8c58
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-02-23 13:29:56 +0300

    Add logic to method parse WordToSymbolParser class

--- src/main/java/by/epam/javatraining/texthandler/service/parser/WordToSymbolParser.java
+++ src/main/java/by/epam/javatraining/texthandler/service/parser/WordToSymbolParser.java
@@ -1,7 +1,11 @@
 package by.epam.javatraining.texthandler.service.parser;
 
+import by.epam.javatraining.texthandler.entity.CharacterLeaf;
 import by.epam.javatraining.texthandler.entity.TextComponent;
 import by.epam.javatraining.texthandler.entity.TextComponentType;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WordToSymbolParser implements ChainTextParser {
 
@@ -11,13 +15,20 @@ public class WordToSymbolParser implements ChainTextParser {
     }
 
     @Override
-    public TextComponent parse(TextComponent textComponent) {
+    public TextComponent parse(TextComponent textComponent, String text) {
         if (textComponent.getTextComponentType() == TextComponentType.WORD) {
-            //parse
+            TextComponent symbol;
+            Pattern pattern = Pattern.compile(TextComponentType.SYMBOL.getComponentRegex());
+            Matcher matcher = pattern.matcher(text);
+
+            while (matcher.find()) {
+                textComponent.addComponent(new CharacterLeaf(matcher.group().charAt(0)));
+            }
+
         } else {
             throw new UnsupportedOperationException();
         }
 
-        return null;
+        return textComponent;
     }
 }
