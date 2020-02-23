76a7ede228326e1ceff3ac6e3c844e98a1a496b9
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-02-23 21:22:39 +0300

    Create ParagraphSorterBySentenceNumber class

--- src/main/java/by/epam/javatraining/texthandler/service/sorter/ParagraphSorterBySentenceNumber.java
+++ src/main/java/by/epam/javatraining/texthandler/service/sorter/ParagraphSorterBySentenceNumber.java
@@ -1,10 +1,9 @@
 package by.epam.javatraining.texthandler.service.sorter;
 
+import by.epam.javatraining.texthandler.comparator.ParagraphComparatorBySentenceNumber;
 import by.epam.javatraining.texthandler.entity.TextComponent;
 import by.epam.javatraining.texthandler.entity.TextComponentType;
 import by.epam.javatraining.texthandler.exception.ServiceException;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class ParagraphSorterBySentenceNumber {
@@ -15,7 +14,6 @@ public class ParagraphSorterBySentenceNumber {
         }
 
         List<TextComponent> paragraphList = textComponent.getComponentList();
-
-        paragraphList.sort();
+        paragraphList.sort(new ParagraphComparatorBySentenceNumber());
     }
 }
