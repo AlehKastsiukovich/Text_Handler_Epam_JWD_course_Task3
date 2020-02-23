840a83c9024aabb3299e842f43d173308eb92e65
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-02-23 13:08:54 +0300

    Add logic to parse method SentenceToLexemeParser class

--- src/main/java/by/epam/javatraining/texthandler/service/parser/SentenceToLexemeParser.java
+++ src/main/java/by/epam/javatraining/texthandler/service/parser/SentenceToLexemeParser.java
@@ -2,6 +2,9 @@ package by.epam.javatraining.texthandler.service.parser;
 
 import by.epam.javatraining.texthandler.entity.TextComponent;
 import by.epam.javatraining.texthandler.entity.TextComponentType;
+import by.epam.javatraining.texthandler.entity.TextComposite;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SentenceToLexemeParser implements ChainTextParser {
     private ChainTextParser nextParser;
@@ -12,13 +15,20 @@ public class SentenceToLexemeParser implements ChainTextParser {
     }
 
     @Override
-    public TextComponent parse(TextComponent textComponent) {
+    public TextComponent parse(TextComponent textComponent, String text) {
         if (textComponent.getTextComponentType() == TextComponentType.SENTENCE) {
-            //parse
+            TextComponent lexeme = new TextComposite(TextComponentType.LEXEME);
+
+            Pattern pattern = Pattern.compile(lexeme.getTextComponentType().getComponentRegex());
+            Matcher matcher = pattern.matcher(text);
+
+            while (matcher.find()) {
+                textComponent.addComponent(nextParser.parse(lexeme, matcher.group()));
+            }
         } else {
-            nextParser.parse(textComponent);
+            nextParser.parse(textComponent, text);
         }
 
-        return null;
+        return textComponent;
     }
 }
